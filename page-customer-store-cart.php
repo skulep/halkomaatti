037695f5c57eccbaf7caa69df1a9e863f26bf3ad
Krymyy <57089665+Krymyy@users.noreply.github.com>
2023-03-29 14:12:02 +0300

    Create page-customer-store-cart.php
    store cart sivu

---
+++ page-customer-store-cart.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Customer Store Cart
+*/
+
+get_header('customer');
+?>
+
+	<main id="primary" class="site-main">
+
+	
+
+
+		
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
