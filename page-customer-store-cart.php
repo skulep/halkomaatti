ab2148d5f638457a2035c534f52cb5e65931ebd3
Krymyy <57089665+Krymyy@users.noreply.github.com>
2023-04-05 13:31:23 +0300

    Customer info page
    Info page started

--- page-customer-store-cart.php
+++ page-customer-store-cart.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Customer Store Cart
+Template Name: Customer Info
 */
 
 get_header('customer');
@@ -8,7 +8,22 @@ get_header('customer');
 
 	<main id="primary" class="site-main">
 
-	
+
+		<div class="container d-grid gap-3">
+			<h2>Contact Us</h2>
+
+		<div class="container border border-5 border-primary" >
+			<div class="panel panel-default">
+				<div class="panel-body">A Basic Panel</div>
+        	</div>
+		</div>
+
+		<div class="container border border-5 border-primary" >
+			<div class="panel panel-default">
+				<div class="panel-body">A Basic Panel</div>
+			</div>
+        </div>
+		</div>
 
 
 		
