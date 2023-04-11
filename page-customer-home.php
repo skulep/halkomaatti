ea448b9b858e7012d37d6b776ca0eef1a06fb2c2
skulep <60211877+skulep@users.noreply.github.com>
2023-04-11 14:41:47 +0300

    customer home fix
    image is not visible on mobile screens, replaced with text

--- page-customer-home.php
+++ page-customer-home.php
@@ -7,20 +7,32 @@ Template Name: Customer Home Page
 			<img src="https://img.freepik.com/free-photo/river-surrounded-by-forests-cloudy-sky-thuringia-germany_181624-30863.jpg?w=1380&t=st=1679647986~exp=1679648586~hmac=3d3071b4c5e43aba1721ff57426f53a7a99857dd14719f6cd14f7a49988680cd" class="img-fluid" width="100%">
 			<p class="text-on-img text-center">In need of wood? Halkomaatti has you covered!</p>
 		</div>
-*/
 
-get_header('customer');
-?>
-
-
-<div class="banner"> 
-	<img src="https://img.freepik.com/free-photo/river-surrounded-by-forests-cloudy-sky-thuringia-germany_181624-30863.jpg?w=1380&t=st=1679647986~exp=1679648586~hmac=3d3071b4c5e43aba1721ff57426f53a7a99857dd14719f6cd14f7a49988680cd" style="max-height: 550px">
+		<div class="banner"> 
+	<div class="banner-img" style="max-height: 550px">
 	<div class="container">
 		<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
 			<div class="home_slider_title text-on-img">In need of wood? Halkomaatti has you covered!</div>
 		</div>
 	</div>
 </div>
+*/
+
+get_header('customer');
+?>
+
+
+
+<div class="banner d-none d-sm-block"> 
+<img src="https://img.freepik.com/free-photo/river-surrounded-by-forests-cloudy-sky-thuringia-germany_181624-30863.jpg?w=1380&t=st=1679647986~exp=1679648586~hmac=3d3071b4c5e43aba1721ff57426f53a7a99857dd14719f6cd14f7a49988680cd" style="width: 100%">
+	<div class="container">
+		<div class="home_slider_content">
+			<div class="home_slider_title text-on-img">In need of wood?<br>Halkomaatti has you covered!</div>
+		</div>
+	</div>
+</div>
+
+<h4 class="d-sm-none text-center">In need of wood?<br>Halkomaatti has you covered!</h4>
 
 <div class="vstack gap-3 col-md-8 mx-auto mt-5">
 	<h5 class="text-center">List of Locations</h5>		
