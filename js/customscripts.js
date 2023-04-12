3bb489e38d68cf68b42e45a6a3dcb523a566e54d
skulep <60211877+skulep@users.noreply.github.com>
2023-04-12 12:32:20 +0300

    fillbox changes, modal not working

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,1 +1,22 @@
 //Custom jQuery or Javascript functions
+
+//Modal -- edit visible text
+
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Currently editing Tuira box no. ' + recipient
+  modalBodyInput.value = recipient
+})
+
+//FillBox -- change box color to red or green
--- page-fillbox.php
+++ page-fillbox.php
@@ -8,65 +8,110 @@ p class: h3 mb-3 fw-normal
 get_header('admin');
 ?>
 
-<main class="form-signin w-150 m-auto">
-  <form>
+<main class="container">
+
     <h4 class="h3 mb-3 fw-normal">Tuira</h4>
 	<p class="">Oulu, Finland</p>
 	<p class="">53.6346, 353.6443</p>
 
-    <div class="form-floating pb-2">
-		<input type="number" class="form-control" id="typeNumber" placeholder="number">
-		<label for="typeNumber">Firewood filled</label>
-    </div>
+    <button type="button" class="btn btn-defaultfont">Info</button>
 
-	
+<div class="container">
+	<p class="text-center">All Boxes</p>
 
-	<div class="row">
-		<p class="">Additional items added:</p>
-		<div class="form-group col-md-4">
-			<label for="inputState">Matches</label>
-			<select id="inputState" class="form-control">
-				<option selected>-</option>
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-			</select>
+	<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+
+
+  	<div class="row pt-5">
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">1
+				<br>
+				<div class="form-check form-switch">
+					<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+				</div>
+			</button>
+			
 		</div>
-		<div class="form-group col-md-4">
-			<label for="inputState">Batteries</label>
-			<select id="inputState" class="form-control">
-				<option selected>-</option>
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-			</select>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">2</button>
 		</div>
-		<div class="form-group col-md-4">
-			<label for="inputState">Paper</label>
-			<select id="inputState" class="form-control">
-				<option selected>-</option>
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-		</select>
-    </div>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">3</button>
+		</div>
   	</div>
 
-
-
-	<div class="form-group">
-		<label for="exampleFormControlTextarea1">Message (optional)</label>
-		<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+	  <div class="row pt-5">
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">4</button>
+		</div>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">5</button>
+		</div>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">6</button>
+		</div>
   	</div>
 
-    <div class="checkbox mb-3">
-		<label>
-				<input type="checkbox" value="remember-me"> Mark as important/critical
-		</label>
-    </div>
-	<a class="w-100 btn btn-lg btn-primary" href="http://localhost/wordpress" role="button">Mark as filled</a> 
-  </form>
+	  <div class="row pt-5 pb-5">
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">7</button>
+		</div>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">8</button>
+		</div>
+		<div class="col d-flex justify-content-center">
+			<button type="button" class="btn btn-outline-grey-border btn-rounded-square">9</button>
+		</div>
+  	</div>
+</div>
+
+<div class="form-group">
+	<label for="exampleFormControlTextarea1">Message (optional)</label>
+	<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+<div class="checkbox mb-3">
+	<label>
+		<input type="checkbox" value="remember-me"> Mark as important/critical
+	</label>
+</div>
+
+<a class="w-100 btn btn-lg btn-primary pb-5" href="http://localhost/wordpress" role="button">Mark as filled</a> 
+
+
+<!---- On-click Modal --->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+      		<div class="modal-header">
+        		<h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        		<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      		</div>
+      		<div class="modal-body">
+				<form>
+					<div class="mb-3">
+						<label for="recipient-name" class="col-form-label">Recipient:</label>
+						<input type="text" class="form-control" id="recipient-name">
+					</div>
+					<div class="mb-3">
+						<label for="message-text" class="col-form-label">Message:</label>
+						<textarea class="form-control" id="message-text"></textarea>
+					</div>
+				</form>
+      		</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Send message</button>
+			</div>
+    	</div>
+	</div>
+</div>
+
 </main>    
+
+
+
+
 
 <?php
 get_sidebar();
